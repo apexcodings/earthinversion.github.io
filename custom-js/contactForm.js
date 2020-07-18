25f3a30dd958a89d7e349f4b1cf47f6734656ef3
Utpal Kumar <utpalkumar50@gmail.com>
2020-07-18 14:03:55 +0800

    automator Sat Jul 18 14:03:55 CST 2020

--- custom-js/contactForm.js
+++ custom-js/contactForm.js
@@ -33,7 +33,7 @@ function sendEmail(e) {
 
     $('#alert-field')
       .removeClass()
-      .html('<progress style="width: 100%;"></progress>')
+      .html('<progress></progress>')
       .removeClass('hidden');
   } else {
     alert('You must set the POST_URL variable with your script ID');
